02a60c78b82cc274def77c86c9794a65f694c411
RaptorKode <tenisdp@gmail.com>
2020-08-11 22:12:29 -0500

    se agrego un inner join para obtener los datos de la tabla de documentos

--- application/models/Ordenes_Trabajo/Servicio_tecnico_model.php
+++ application/models/Ordenes_Trabajo/Servicio_tecnico_model.php
@@ -8,6 +8,7 @@
 			$this->db->select('*');
 			$this->db->from('ot_servicio_tecnico');
 			$this->db->join('cliente', 'cliente.ID_Cliente = ot_servicio_tecnico.ID_Cliente');
+			$this->db->join('tipo_documento', 'tipo_documento.ID_Documento = ot_servicio_tecnico.ID_Documento');
 			$this->db->order_by('ID_OTServicioTecnico', 'ASC');
 			$query = $this->db->get();
 			return $query->result();
