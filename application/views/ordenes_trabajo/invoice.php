343cba996e1b56fb9cc567592a65612e3e78bfdb
RaptorKode <tenisdp@gmail.com>
2020-08-12 14:54:55 -0500

    se edito la factura con los campos respectivos que deberia mostrarse

--- application/views/ordenes_trabajo/invoice.php
+++ application/views/ordenes_trabajo/invoice.php
@@ -8,8 +8,8 @@
 						 alt=""
 						 style="width: 120px;">
 					<ul class="list list-unstyled mb-0">
+						<li>Julio César Paucar</li>
 						<li>Jacinto Escobar Oe4-31 pasaje S21A y José Abarcas</li>
-						<li>2269 Elba Lane</li>
 						<li>Quito - Ecuador</li>
 						<li>0984035994</li>
 					</ul>
@@ -19,7 +19,8 @@
 			<div class="col-sm-6">
 				<div class="mb-4">
 					<div class="text-sm-right">
-						<h4 class="text-orange-300 mb-2 mt-md-2">Invoice #49029</h4>
+						<h4 class="text-orange-300 mb-2 mt-md-2"><?= $venta->Nombre_Documento ?> #<?=
+								$venta->NumeroDocumento_OTServicioTecnico ?></h4>
 						<ul class="list list-unstyled mb-0">
 							<?php
 								$fechaOriginal = $venta->Fecha_OTServicioTecnico;
@@ -50,26 +51,26 @@
 			</div>
 			
 			<div class="mb-2 ml-auto">
-				<span class="text-muted">Payment Details:</span>
+				<span class="text-muted">Detalles:</span>
 				<div class="d-flex flex-wrap wmin-md-400">
 					<ul class="list list-unstyled mb-0">
-						<li><h5 class="my-2">Total Due:</h5></li>
-						<li>Bank name:</li>
-						<li>Country:</li>
-						<li>City:</li>
-						<li>Address:</li>
-						<li>IBAN:</li>
-						<li>SWIFT code:</li>
+						<li><h5 class="my-2">Total a Pagar:</h5></li>
+						<!--						<li>Bank name:</li>-->
+						<!--						<li>Country:</li>-->
+						<!--						<li>City:</li>-->
+						<!--						<li>Address:</li>-->
+						<!--						<li>IBAN:</li>-->
+						<!--						<li>SWIFT code:</li>-->
 					</ul>
 					
 					<ul class="list list-unstyled text-right mb-0 ml-auto">
-						<li><h5 class="font-weight-semibold my-2">$8,750</h5></li>
-						<li><span class="font-weight-semibold">Profit Bank Europe</span></li>
-						<li>United Kingdom</li>
-						<li>London E1 8BF</li>
-						<li>3 Goodman Street</li>
-						<li><span class="font-weight-semibold">KFH37784028476740</span></li>
-						<li><span class="font-weight-semibold">BPT4E</span></li>
+						<li><h5 class="font-weight-semibold my-2">$<?= $venta->Total_OTServicioTecnico ?></h5></li>
+						<!--						<li><span class="font-weight-semibold">Profit Bank Europe</span></li>-->
+						<!--						<li>United Kingdom</li>-->
+						<!--						<li>London E1 8BF</li>-->
+						<!--						<li>3 Goodman Street</li>-->
+						<!--						<li><span class="font-weight-semibold">KFH37784028476740</span></li>-->
+						<!--						<li><span class="font-weight-semibold">BPT4E</span></li>-->
 					</ul>
 				</div>
 			</div>
@@ -80,40 +81,40 @@
 		<table class="table table-lg">
 			<thead>
 				<tr>
-					<th>Description</th>
-					<th>Rate</th>
-					<th>Hours</th>
+					<th>Descripción</th>
+					<th>Marca</th>
+					<th>Modelo</th>
 					<th>Total</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
 					<td>
-						<h6 class="mb-0">Create UI design model</h6>
-						<span class="text-muted">One morning, when Gregor Samsa woke from troubled.</span>
+						<h6 class="mb-0"><?= $venta->Descripcion_OTServicioTecnico ?></h6>
+						<!--						<span class="text-muted">One morning, when Gregor Samsa woke from troubled.</span>-->
 					</td>
-					<td>$70</td>
-					<td>57</td>
-					<td><span class="font-weight-semibold">$3,990</span></td>
+					<td><?= $venta->Marca_OTServicioTecnico ?></td>
+					<td><?= $venta->Modelo_OTServicioTecnico ?></td>
+					<td><span class="font-weight-semibold">$<?= $detalle->Precio_DetalleOTServicioTecnico ?></span></td>
 				</tr>
-				<tr>
-					<td>
-						<h6 class="mb-0">Support tickets list doesn't support commas</h6>
-						<span class="text-muted">I'd have gone up to the boss and told him just what i think.</span>
-					</td>
-					<td>$70</td>
-					<td>12</td>
-					<td><span class="font-weight-semibold">$840</span></td>
-				</tr>
-				<tr>
-					<td>
-						<h6 class="mb-0">Fix website issues on mobile</h6>
-						<span class="text-muted">I am so happy, my dear friend, so absorbed in the exquisite.</span>
-					</td>
-					<td>$70</td>
-					<td>31</td>
-					<td><span class="font-weight-semibold">$2,170</span></td>
-				</tr>
+				<!--				<tr>-->
+				<!--					<td>-->
+				<!--						<h6 class="mb-0">Support tickets list doesn't support commas</h6>-->
+				<!--						<span class="text-muted">I'd have gone up to the boss and told him just what i think.</span>-->
+				<!--					</td>-->
+				<!--					<td>$70</td>-->
+				<!--					<td>12</td>-->
+				<!--					<td><span class="font-weight-semibold">$840</span></td>-->
+				<!--				</tr>-->
+				<!--				<tr>-->
+				<!--					<td>-->
+				<!--						<h6 class="mb-0">Fix website issues on mobile</h6>-->
+				<!--						<span class="text-muted">I am so happy, my dear friend, so absorbed in the exquisite.</span>-->
+				<!--					</td>-->
+				<!--					<td>$70</td>-->
+				<!--					<td>31</td>-->
+				<!--					<td><span class="font-weight-semibold">$2,170</span></td>-->
+				<!--				</tr>-->
 			</tbody>
 		</table>
 	</div>
@@ -121,7 +122,7 @@
 	<div class="card-body">
 		<div class="d-md-flex flex-md-wrap">
 			<div class="pt-2 mb-3">
-				<h6 class="mb-3">Authorized person</h6>
+				<h6 class="mb-3">Firma Autorizada</h6>
 				<div class="mb-3">
 					<img src="<?= base_url() ?>components/global_assets/images/signature_light.png"
 						 width="150"
@@ -129,46 +130,36 @@
 				</div>
 				
 				<ul class="list-unstyled text-muted">
-					<li>Eugene Kopyov</li>
-					<li>2269 Elba Lane</li>
-					<li>Paris, France</li>
-					<li>888-555-2311</li>
+					<li>Julio César Paucar</li>
+					<li>Jacinto Escobar Oe4-31 pasaje S21A y José Abarcas</li>
+					<li>Quito - Ecuador</li>
+					<li>0984035994</li>
 				</ul>
 			</div>
 			
 			<div class="pt-2 mb-3 wmin-md-400 ml-auto">
-				<h6 class="mb-3">Total due</h6>
+				<h6 class="mb-3">Total a Pagar:</h6>
 				<div class="table-responsive">
 					<table class="table">
 						<tbody>
 							<tr>
 								<th>Subtotal:</th>
-								<td class="text-right">$7,000</td>
+								<td class="text-right">$<?= $venta->Subtotal_OTServicioTecnico ?></td>
 							</tr>
 							<tr>
-								<th>Tax: <span class="font-weight-normal">(25%)</span></th>
-								<td class="text-right">$1,750</td>
+								<th>Impuesto: <span class="font-weight-normal"><?= $venta->Impuesto_Documento . '%'
+										?></span></th>
+								<td class="text-right">$<?= $venta->Impuesto_OTServicioTecnico ?></td>
 							</tr>
 							<tr>
 								<th>Total:</th>
 								<td class="text-right text-orange-300"><h5 class="font-weight-semibold">
-										$8,750</h5></td>
+										$<?= $venta->Total_OTServicioTecnico ?></h5></td>
 							</tr>
 						</tbody>
 					</table>
 				</div>
-				
-				<div class="text-right mt-3">
-					<button type="button"
-							class="btn btn-primary btn-labeled btn-labeled-left">
-						<b><i class="icon-paperplane"></i></b> Send invoice
-					</button>
-				</div>
 			</div>
 		</div>
-	</div>
-	
-	<div class="card-footer border-light">
-		<span class="text-muted">Thank you for using Limitless. This invoice can be paid via PayPal, Bank transfer, Skrill or Payoneer. Payment is due within 30 days from the date of delivery. Late payment is possible, but with with a fee of 10% per month. Company registered in England and Wales #6893003, registered office: 3 Goodman Street, London E1 8BF, United Kingdom. Phone number: 888-555-2311</span>
 	</div>
 </div>
