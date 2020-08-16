4c0b76336fb8e67d5dbd3ccf3c5f36f9e0994497
RaptorKode <tenisdp@gmail.com>
2020-08-16 18:40:49 -0500

    se cambio el idioma de la tabla y se le agrego estetica

--- components/scripts/servicio_tecnico.js
+++ components/scripts/servicio_tecnico.js
@@ -314,10 +314,47 @@ $(document).ready(function () {
 	$('#tablaServicioTecnico').DataTable({
 		
 		'ajax': "http://localhost/ci3/ordenes_trabajo/servicio_tecnico/mostrar",
+		dom: '<"datatable-header"fl><"datatable-scroll-wrap"t><"datatable-footer"ip>',
 		order: [],
 		responsive: true,
-		autoWidth: false
+		autoWidth: false,
+		processing: true,
+		language: {
+			"sProcessing": "Procesando...",
+			"sZeroRecords": "No se encontraron resultados",
+			"sEmptyTable": "Ningún dato disponible en esta tabla",
+			"sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+			"sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+			"sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+			"sInfoPostFix": "",
+			"sUrl": "",
+			"sInfoThousands": ",",
+			"sLoadingRecords": "Cargando...",
+			"oAria": {
+				"sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+				"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+			},
+			// Select2 for length menu styling
+			
+			// Initialize
+			
+			search: '<span>Filtro:</span> _INPUT_',
+			searchPlaceholder: 'Escriba para buscar...',
+			lengthMenu: '<span>Mostrar:</span> _MENU_',
+			paginate: {
+				'first': 'First',
+				'last': 'Last',
+				'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;',
+				'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;'
+			}
+		},
+		
 	})
+	$('.dataTables_length select').select2({
+		minimumResultsForSearch: Infinity,
+		dropdownAutoWidth: true,
+		width: 'auto'
+	});
 })
 
 /* ELIMINAR ORDEN DE TRABAJO SERVICIO TECNICO */
