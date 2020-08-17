4b25d2c74ec11e58694367a50e25dd273ae356ba
RaptorKode <tenisdp@gmail.com>
2020-08-17 17:01:05 -0500

    se inicializo el plugin select2 para la seleccion de documentos

---
+++ components/scripts/ploteo.js
@@ -0,0 +1,6 @@
+/* PLUGINS SELECT2 EN EL SELECTOR DE DOCUMENTO EN EL FORMULARIO DE GUARDAR ORDEN DE TRABAJO PLOTEO */
+$('#tipoDocumentoPloteo').select2({
+	placeholder: 'Seleccione un documento',
+	allowClear: true,
+	minimumResultsForSearch: Infinity,
+})
