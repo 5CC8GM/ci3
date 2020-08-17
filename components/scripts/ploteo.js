9903e8b4806274f6929b726e8db5ef3c7be62bbd
RaptorKode <tenisdp@gmail.com>
2020-08-17 18:14:07 -0500

    se creo el procedimientoa para que obtener los datos del documento seleccionado por el id

--- components/scripts/ploteo.js
+++ components/scripts/ploteo.js
@@ -4,3 +4,23 @@ $('#tipoDocumentoPloteo').select2({
 	allowClear: true,
 	minimumResultsForSearch: Infinity,
 })
+
+/* FUNCION PARA EDITAR LOS PRECIOS EN EL FORMULARIO GUARDAR SERVICIO TECNICO EN BASE AL DOCUMENTO SELECCIONADO */
+$('#tipoDocumentoPloteo').change(function () {
+	
+	/* PONER EN UNA VARIABLE EL CONTENIDO DEL SELECTOR */
+	let idDocumentoPloteo = $(this).val();
+	// alert(idDocumentoPloteo)
+	$.ajax({
+		url: 'http://localhost/ci3/ploteo/obtenerDocumentos',
+		type: 'post',
+		data: {
+			id: idDocumentoPloteo
+		},
+		dataType: 'json',
+		success: function (data) {
+			// console.log(data)
+			
+		}
+	})
+})
