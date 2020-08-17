284f6ad098518c9840aee1a8d15039e2b439217d
RaptorKode <tenisdp@gmail.com>
2020-08-17 17:09:47 -0500

    se creo la funcion para obtener todos los documentos desde la base de datos

--- application/models/Ordenes_Trabajo/Ploteo_model.php
+++ application/models/Ordenes_Trabajo/Ploteo_model.php
@@ -2,6 +2,14 @@
 	defined('BASEPATH') or exit('No direct script access allowed');
 	
 	class Ploteo_model extends CI_Model {
-	
-	
+		
+		/* FUNCION PARA OBTENER TODOS LOS DOCUMENTOS DE LA BASE DE DATOS */
+		public function getComprobantes() {
+			
+			$resultado = $this->db->get('tipo_documento');
+			
+			return $resultado->result();
+			
+		}
+		
 	}
