4d76eb45fb6f4fce15e60b5ba3ad74a232997f7c
RaptorKode <tenisdp@gmail.com>
2020-08-19 22:19:29 -0500

    se corrigieron los inputs del modal para editar la orden de trabajo ploteo asi como sus nombres e identificadores

--- application/views/ordenes_trabajo/ploteo.php
+++ application/views/ordenes_trabajo/ploteo.php
@@ -188,13 +188,13 @@
 	</div>
 </div>
 <!-- MODAL EDITAR ORDEN DE TRABAJO SERVICIO TECNICO -->
-<div id="modalEditarOtServicioTecnico"
+<div id="modalEditarOtPloteo"
 	 class="modal fade"
 	 tabindex="-1">
 	<div class="modal-dialog modal-sm">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Editar Orden de Trabajo Servicio Técnico</h5>
+				<h5 class="modal-title">Editar Orden de Trabajo Ploteo</h5>
 				<button type="button"
 						class="close"
 						data-dismiss="modal">&times;
@@ -203,17 +203,18 @@
 			<div class="modal-body">
 				<form action=""
 					  method="post"
-					  id="formularioServicioTecnico">
+					  id="formularioPloteo">
 					<div class="form-group row">
 						<div class="col-sm-6">
-							<label for="tipoDocumento">Tipo de Documento</label>
-							<select id="tipoDocumento"
-									name="tipoDocumento"
+							<label for="editarTipoDocumentoPloteo">Tipo de Documento</label>
+							<select id="editarTipoDocumentoPloteo"
+									name="editarTipoDocumentoPloteo"
 									class="form-control"
 									data-fouc
-									required>
+									required
+									disabled="disabled">
 								<option></option>
-								<?php foreach ($tipoDocumento as $documento): ?>
+								<?php foreach ($tipoDocumentoPloteo as $documento): ?>
 									
 									<?php $dataDocumento = $documento->ID_Documento . '*'
 										. $documento->Cantidad_Documento . '*' .
@@ -223,41 +224,45 @@
 								<?php endforeach; ?>
 							</select>
 							<input type="hidden"
-								   id="infoOculta">
+								   id="editarDataDocumentoPloteo"
+								   name="editarDataDocumentoPloteo">
 							<input type="hidden"
-								   id="idDocumento"
-								   name="idDocumento">
+								   id="editarIdDocumentoPloteo"
+								   name="editarIdDocumentoPloteo">
 							<input type="hidden"
-								   id="impuestoDocumento"
-								   name="impuestoDocumento">
+								   id="editarImpuestoDocumentoPloteo"
+								   name="editarImpuestoDocumentoPloteo">
+							<input type="hidden"
+								   id="editarIdOtPloteo"
+								   name="editarIdOtPloteo">
 						</div>
 						<div class="col-sm-3">
-							<label for="serieDocumento">Serie</label>
+							<label for="editarSerieDocumentoPloteo">Serie</label>
 							<input type="text"
-								   name="serieDocumento"
-								   id="serieDocumento"
+								   name="editarSerieDocumentoPloteo"
+								   id="editarSerieDocumentoPloteo"
 								   class="form-control"
 								   readonly>
 						</div>
 						<div class="col-sm-3">
-							<label for="numeroDocumento">Número</label>
+							<label for="editarNumeroDocumentoPloteo">Número</label>
 							<input type="text"
-								   name="numeroDocumento"
-								   id="numeroDocumento"
+								   name="editarNumeroDocumentoPloteo"
+								   id="editarNumeroDocumentoPloteo"
 								   class="form-control"
 								   readonly>
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="cliente">Cliente</label>
-						<select id="cliente"
-								name="cliente"
+						<label for="editarClientePloteo">Cliente</label>
+						<select id="editarClientePloteo"
+								name="editarClientePloteo"
 								class="form-control select-search"
 								data-placeholder="Seleccione un cliente"
 								data-fouc
 								required>
 							<option></option>
-							<?php foreach ($cliente as $value): ?>
+							<?php foreach ($clientePloteo as $value): ?>
 								
 								<option value="<?= $value->ID_Cliente ?>"><?= $value->Nombre_Cliente . ' ' . $value->Apellido_Cliente ?></option>'
 							
@@ -265,77 +270,63 @@
 						</select>
 					</div>
 					<div class="form-group row">
-						<div class="col-md-6">
-							<label for="marca">Marca:</label>
+						<div class="col-sm-5">
+							<label for="editarMetrosPloteo">Metros de Ploteo</label>
 							<input type="text"
+								   name="editarMetrosPloteo"
+								   id="editarMetrosPloteo"
 								   class="form-control"
-								   name="marca"
-								   id="marca"
-								   placeholder="Asus"
-								   required>
+								   placeholder="1.75">
 						</div>
-						<div class="col-md-6">
-							<label for="modelo">Modelo:</label>
-							<input type="text"
-								   class="form-control"
-								   name="modelo"
-								   id="modelo"
-								   placeholder="BHMX350"
-								   required>
+						<div class="col-sm-7 align-self-end text-right">
+							<button href="#"
+									id="editarAgregarPloteo"
+									class="btn btn-block btn-success btn-icon"
+									type="button"><i class="icon-trash"></i> Agregar
+							</button>
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="descripcion">Descripción:</label>
-						<textarea rows="1"
-								  cols="1"
-								  class="form-control elastic"
-								  placeholder="Descripción:"
-								  id="descripcion"
-								  name="descripcion"
-								  required></textarea>
-					</div>
-					<div class="form-group row">
-						<div class="col-md-6">
-							<label for="precio">Precio:</label>
-							<input type="text"
-								   class="input form-control"
-								   name="precio"
-								   id="precio"
-								   placeholder="30.50"
-								   required>
-						</div>
-						<div class="col-md-6">
-							<label for="iva">IVA:</label>
-							<div class="input-group">
-								<input type="text"
-									   id="iva"
-									   name="iva"
-									   class="input form-control"
-									   readonly>
-								<span class="input-group-append">
-									<span class="input-group-text">IVA</span>
-								</span>
-							</div>
+						<div class="table-responsive">
+							<table class="table table-bordered tablaEditarAgregarPloteo">
+								<thead>
+									<tr>
+										<th>Metros</th>
+										<th>Precio Final</th>
+										<th>Acción</th>
+									</tr>
+								</thead>
+								<tbody>
+								
+								</tbody>
+							</table>
 						</div>
 					</div>
 					<div class="form-group row">
-						<div class="col-md-6 ml-md-auto">
-							<label for="subtotal">Subtotal:</label>
+						<div class="col-md-4">
+							<label for="editarSubtotalPloteo">Subtotal:</label>
 							<input type="text"
 								   class="input form-control"
-								   name="subtotal"
-								   id="subtotal"
+								   name="editarSubtotalPloteo"
+								   id="editarSubtotalPloteo"
 								   readonly
 								   required>
 						</div>
-					</div>
-					<div class="form-group row">
-						<div class="col-md-6 ml-md-auto">
-							<label for="total">Total:</label>
+						<div class="col-md-4">
+							<label for="editarIvaPloteo">IVA:</label>
 							<input type="text"
 								   class="input form-control"
-								   name="total"
-								   id="total"
+								   name="editarIvaPloteo"
+								   id="editarIvaPloteo"
+								   readonly
+								   required>
+						</div>
+						<div class="col-md-4">
+							<label for="editarTotalPloteo">Total:</label>
+							<input type="text"
+								   class="input form-control"
+								   name="editarTotalPloteo"
+								   id="editarTotalPloteo"
 								   readonly
 								   required>
 						</div>
@@ -343,7 +334,7 @@
 					<div class="text-center">
 						<button class="btn bg-primary-800"
 								type="button"
-								id="crearOrdenTrabajoServicioTecnico">Crear Orden de Trabajo <i
+								id="editarOrdenTrabajoPloteo">Editar Orden de Trabajo <i
 									class="icon-paperplane
 									ml-2"></i></button>
 					</div>
