357e6c3fcf2a3dfb9a2f2e4ae26ab40a5507755e
RaptorKode <tenisdp@gmail.com>
2020-09-03 10:53:39 -0500

    :sparkles: breadcrumbs
    se crearon los breadcrumbs respectivos para cada pagina

--- application/views/layouts/breadcrumb.php
+++ application/views/layouts/breadcrumb.php
@@ -1,17 +1,51 @@
 <!-- Page header -->
 <div class="page-header border-bottom-0">
 	<div class="page-header-content header-elements-md-inline">
-		<div class="page-title d-flex">
-			<h4><i class="icon-arrow-left52 mr-2"></i>
-				<span class="font-weight-semibold">Inicio</span></h4>
-			<a href="#"
-			   class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
+		<div class="page-title">
+			<h5>
+				
+				<?php if (current_url() == base_url() . 'dashboard') : ?>
+					<span class="font-weight-semibold">Inicio</span>
+					
+				<?php elseif (current_url() == base_url() . 'clientes') : ?>
+					<span class="font-weight-semibold">Gestión de Clientes</span>
+					
+				<?php elseif (current_url() == base_url() . 'servicio_tecnico') : ?>
+					<span class="font-weight-semibold">Órdenes de Trabajo - Servicio Técnico</span>
+					
+				<?php elseif (current_url() == base_url() . 'ploteo') : ?>
+					<span class="font-weight-semibold">Órdenes de Trabajo - Ploteo</span>
+					
+				<?php endif ?>
+			</h5>
 		</div>
 		
-		<div class="header-elements py-0 mb-3">
+		<div class="header-elements py-0">
 			<div class="breadcrumb">
-				<a href="<?= base_url() ?>dashboard"
-				   class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Inicio</a>
+				
+				<?php if (current_url() == base_url() . 'dashboard') : ?>
+					<a href="<?= base_url() ?>dashboard"
+					   class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Inicio</a>
+				
+				<?php elseif (current_url() == base_url() . 'clientes') : ?>
+					<a href="<?= base_url() ?>dashboard"
+					   class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Inicio</a>
+					<span class="breadcrumb-item active">Clientes</span>
+				
+				<?php elseif (current_url() == base_url() . 'servicio_tecnico') : ?>
+					<a href="<?= base_url() ?>dashboard"
+					   class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Inicio</a>
+					<span class="breadcrumb-item active">Órdenes de Trabajo</span>
+					<span class="breadcrumb-item active">Servicio Tecnico</span>
+				
+				<?php elseif (current_url() == base_url() . 'ploteo') : ?>
+					<a href="<?= base_url() ?>dashboard"
+					   class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Inicio</a>
+					<span class="breadcrumb-item active">Órdenes de Trabajo</span>
+					<span class="breadcrumb-item active">Ploteo</span>
+				
+				<?php endif ?>
+			
 			</div>
 		</div>
 	</div>
