84048b9e5cd4d8d96185f8acb92d73f213425a66
RaptorKode <tenisdp@gmail.com>
2020-09-25 18:47:59 -0500

    :sparkles: modelo mostrar ot servicio tecnico
    se creo la funcion obtener los datos de la base de datos de ot servicio tecnico

--- application/models/Ordenes_Trabajo/Reportes_model.php
+++ application/models/Ordenes_Trabajo/Reportes_model.php
@@ -1,1 +1,15 @@
 <?php
+	defined('BASEPATH') or exit('No direct script access allowed');
+	
+	class Reportes_model extends CI_Model {
+		/* MOSTRAR ORDEN DE TRABAJO SERVICIO TECNICO */
+		public function mostrarServicioTecnico() {
+			$this->db->select('*');
+			$this->db->from('ot_servicio_tecnico');
+			$this->db->join('cliente', 'cliente.ID_Cliente = ot_servicio_tecnico.ID_Cliente');
+			$this->db->join('tipo_documento', 'tipo_documento.ID_Documento = ot_servicio_tecnico.ID_Documento');
+			$this->db->order_by('ID_OTServicioTecnico', 'ASC');
+			$query = $this->db->get();
+			return $query->result_array();
+		}
+	}
