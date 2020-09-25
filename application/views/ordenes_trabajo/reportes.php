dd64bbd4f5fc0c1fe4375db78216c45a84d06903
RaptorKode <tenisdp@gmail.com>
2020-09-25 16:43:41 -0500

    :art: creacion de tabala
    se creo la tabla de reportes

--- application/views/ordenes_trabajo/reportes.php
+++ application/views/ordenes_trabajo/reportes.php
@@ -1,11 +1,37 @@
 <!-- Content area -->
 <div class="content pt-0">
 	<div class="row">
-		<div class="col-xl-4">
-		
-		</div>
-		<div class="col-xl-8">
-		
+		<div class="col-xl-12">
+			<div class="card">
+				<div class="card-header header-elements-inline">
+					<h5 class="card-title">Reporte de las Órdenes de Trabajo</h5>
+					<div class="header-elements">
+						<div class="list-icons">
+							<a class="list-icons-item"
+							   data-action="collapse"></a>
+							<a class="list-icons-item"
+							   data-action="remove"></a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="card-body">
+					<table class="table"
+						   id="tablaReportes">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Cliente</th>
+								<th>Tipo de Documento</th>
+								<th>Número de Documento</th>
+								<th>Fecha</th>
+								<th>Total</th>
+								<th>Acciones</th>
+							</tr>
+						</thead>
+					</table>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
