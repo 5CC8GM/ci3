cbc42ce4f5c7229f2a96a5cf2450fd84bb1db4d5
RaptorKode <tenisdp@gmail.com>
2020-09-25 18:46:24 -0500

    :art: creacion de tabs
    se crearon los tabs para ver los reportes mas ordenados y se creo la tabla para los reportes de ordenes de trabajo

--- application/views/ordenes_trabajo/reportes.php
+++ application/views/ordenes_trabajo/reportes.php
@@ -16,20 +16,52 @@
 				</div>
 				
 				<div class="card-body">
-					<table class="table"
-						   id="tablaReportes">
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Cliente</th>
-								<th>Tipo de Documento</th>
-								<th>Número de Documento</th>
-								<th>Fecha</th>
-								<th>Total</th>
-								<th>Acciones</th>
-							</tr>
-						</thead>
-					</table>
+					<ul class="nav nav-tabs nav-tabs-top nav-justified">
+						<li class="nav-item"><a href="#reporteServicioTecnico" class="nav-link active" data-toggle="tab">Reporte Servicio Técnico</a></li>
+						<li class="nav-item"><a href="#reportePloteo" class="nav-link" data-toggle="tab">Reporte Ploteo</a></li>
+					</ul>
+					
+					<div class="tab-content">
+						<div class="tab-pane fade show active" id="reporteServicioTecnico">
+							
+								<div class="card-body">
+									<table class="table"
+										   id="tablaReporteServicioTecnico">
+										<thead>
+											<tr>
+												<th>#</th>
+												<th>Cliente</th>
+												<th>Tipo de Documento</th>
+												<th>Número de Documento</th>
+												<th>Descripción</th>
+												<th>Fecha</th>
+												<th>Total</th>
+												<th>Acciones</th>
+											</tr>
+										</thead>
+									</table>
+								</div>
+						</div>
+						
+						<div class="tab-pane fade" id="reportePloteo">
+							<div class="card-body">
+								<table class="table"
+									   id="tablaReporPloteo">
+									<thead>
+										<tr>
+											<th>#</th>
+											<th>Cliente</th>
+											<th>Tipo de Documento</th>
+											<th>Número de Documento</th>
+											<th>Fecha</th>
+											<th>Total</th>
+											<th>Acciones</th>
+										</tr>
+									</thead>
+								</table>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
