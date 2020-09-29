a7c52bd7680142ddf71695b0eaa42182252bd7a5
RaptorKode <tenisdp@gmail.com>
2020-09-29 16:08:55 -0500

    :art: revision de codigo
    revision y formateo de codigo

--- application/views/ordenes_trabajo/reportes.php
+++ application/views/ordenes_trabajo/reportes.php
@@ -17,33 +17,39 @@
 				
 				<div class="card-body">
 					<ul class="nav nav-tabs nav-tabs-top nav-justified">
-						<li class="nav-item"><a href="#reporteServicioTecnico" class="nav-link active" data-toggle="tab">Reporte Servicio Técnico</a></li>
-						<li class="nav-item"><a href="#reportePloteo" class="nav-link" data-toggle="tab">Reporte Ploteo</a></li>
+						<li class="nav-item"><a href="#reporteServicioTecnico"
+												class="nav-link active"
+												data-toggle="tab">Reporte Servicio Técnico</a></li>
+						<li class="nav-item"><a href="#reportePloteo"
+												class="nav-link"
+												data-toggle="tab">Reporte Ploteo</a></li>
 					</ul>
 					
 					<div class="tab-content">
-						<div class="tab-pane fade show active" id="reporteServicioTecnico">
+						<div class="tab-pane fade show active"
+							 id="reporteServicioTecnico">
 							
-								<div class="card-body">
-									<table class="table"
-										   id="tablaReporteServicioTecnico">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>Cliente</th>
-												<th>Tipo de Documento</th>
-												<th>Número de Documento</th>
-												<th>Descripción</th>
-												<th>Fecha</th>
-												<th>Total</th>
-												<th>Acciones</th>
-											</tr>
-										</thead>
-									</table>
-								</div>
+							<div class="card-body">
+								<table class="table"
+									   id="tablaReporteServicioTecnico">
+									<thead>
+										<tr>
+											<th>#</th>
+											<th>Cliente</th>
+											<th>Tipo de Documento</th>
+											<th>Número de Documento</th>
+											<th>Descripción</th>
+											<th>Fecha</th>
+											<th>Total</th>
+											<th>Acciones</th>
+										</tr>
+									</thead>
+								</table>
+							</div>
 						</div>
 						
-						<div class="tab-pane fade" id="reportePloteo">
+						<div class="tab-pane fade"
+							 id="reportePloteo">
 							<div class="card-body">
 								<table class="table"
 									   id="tablaReportePloteo">
